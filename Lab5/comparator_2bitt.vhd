d0e7db4dfc190e93be32c15699229ec54fa3dae0
BisheshK <bkachhapati1@gmail.com>
2026-07-06 18:23:56 +0545

    Lab5 Reupload

--- Lab5/comparator_2bitt.vhd
+++ Lab5/comparator_2bitt.vhd
@@ -1,15 +1,25 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-entity comparator_2bitt is
-    Port ( A   : in  STD_LOGIC_VECTOR (1 downto 0);
-           B   : in  STD_LOGIC_VECTOR (1 downto 0);
-           greater : out STD_LOGIC;
-           equal   : out STD_LOGIC;
-           less    : out STD_LOGIC);
-end comparator_2bitt;
-architecture Dataflow of comparator_2bitt is
+use IEEE.NUMERIC_STD.ALL;
+
+entity COMPARATOR_2BITT is
+    port (
+        A  : in  std_logic_vector(1 downto 0);
+        B  : in  std_logic_vector(1 downto 0);
+        EQ : out std_logic; -- A = B
+        GT : out std_logic; -- A > B
+        LT : out std_logic  -- A < B
+    );
+end entity COMPARATOR_2BITT;
+
+architecture Behavioral of COMPARATOR_2BITT is
 begin
-    greater <= '1' when (A > B) else '0';
-    equal   <= '1' when (A = B) else '0';
-    less    <= '1' when (A < B) else '0';
-end Dataflow;
+    process(A, B)
+    begin
+        EQ <= not (A(1) xor B(1)) and not (A(0) xor B(0));
+
+        GT <= (A(1) and not B(1)) or (not (A(1) xor B(1)) and A(0) and not B(0));
+
+        LT <= (not A(1) and B(1)) or (not (A(1) xor B(1)) and not A(0) and B(0));
+    end process;
+end architecture Behavioral;
--- Lab5/comparator_2bitt_tb.vhd
+++ Lab5/comparator_2bitt_tb.vhd
@@ -1,44 +1,31 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
-entity comparator_2bitt_tb is
-end comparator_2bitt_tb;
+entity COMPARATOR_2BITT_TB is
+end entity COMPARATOR_2BITT_TB;
 
-architecture Behavioral of comparator_2bitt_tb is
-    component comparator_2bitt
-        Port ( A       : in  STD_LOGIC_VECTOR (1 downto 0);
-               B       : in  STD_LOGIC_VECTOR (1 downto 0);
-               greater : out STD_LOGIC;
-               equal   : out STD_LOGIC;
-               less    : out STD_LOGIC);
-    end component;
-    signal A_tb : STD_LOGIC_VECTOR(1 downto 0) := "00";
-    signal B_tb : STD_LOGIC_VECTOR(1 downto 0) := "00";
-    signal greater_tb : STD_LOGIC;
-    signal equal_tb   : STD_LOGIC;
-    signal less_tb    : STD_LOGIC;
-
+architecture Simulation of COMPARATOR_2BITT_TB is
+    signal A, B : std_logic_vector(1 downto 0) := "00";
+    signal EQ, GT, LT : std_logic;
 begin
-
-    uut: comparator_2bitt PORT MAP (
-          A => A_tb,
-          B => B_tb,
-          greater => greater_tb,
-          equal => equal_tb,
-          less => less_tb
+    DUT: entity work.COMPARATOR_2BITT
+        port map (
+            A  => A,
+            B  => B,
+            EQ => EQ,
+            GT => GT,
+            LT => LT
         );
 
-    stim_proc: process
-    begin		
-        for i in 0 to 3 loop
-            for j in 0 to 3 loop
-                A_tb <= std_logic_vector(to_unsigned(i, 2));
-                B_tb <= std_logic_vector(to_unsigned(j, 2));
-                wait for 10 ns;
-            end loop;
-        end loop;
-        wait;
+    STIMULUS: process
+    begin
+        A <= "00"; B <= "00"; wait for 10 ns; -- EQ = 1
+        A <= "01"; B <= "00"; wait for 10 ns; -- GT = 1
+        A <= "00"; B <= "01"; wait for 10 ns; -- LT = 1
+        A <= "10"; B <= "11"; wait for 10 ns; -- LT = 1
+        A <= "11"; B <= "10"; wait for 10 ns; -- GT = 1
+        A <= "11"; B <= "11"; wait for 10 ns; -- EQ = 1
+        
+        wait; -- End of simulation
     end process;
-
-end Behavioral;
+end architecture Simulation;
